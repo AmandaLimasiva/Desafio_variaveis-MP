983dbfd14b75c3ed822d8ba592f169754eced0ff
Amanda Lima <51892828+AmandaLimasiva@users.noreply.github.com>
2022-05-30 21:31:02 -0300

    Subindo Desafio JS

---
+++ script.js
@@ -0,0 +1,20 @@
+//Variáveis
+
+//String
+
+let nome = "Amanda";
+
+//Inteiro
+
+let n = 1;
+
+// Double e float
+
+let pi = 3.14;
+let nF = 1.72;
+
+// Boolean 
+
+let verdade = true;
+
+let falso = false;
